134b7535f75f678d85c7513df58690a29fab18a8
NataCj <mncardenasj@ufpso.edu.co>
2023-11-29 15:40:36 -0500

    todo_tailwind first commit

---
+++ todo_tailwind/todo_tailwind/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import TaskComponent from '@/components/TaskComponent.vue';
+</script>
+
+<template>
+<div class="px-4 py-2 w-96 bg-[#F2E3D5] shadow-md rounded mx-auto mt-10">
+  <h1 class="text-2xl text-center font-semibold text-[#012E40]">Lista de Tareas</h1>
+
+  <div class="flex mt-2">
+    <input type="text" class="outline-none w-4/5 h-8 px-2 py-1 border border-[#012E40] rounded-r-none rounded">
+    <span class="transition-colors duration-200 hover:cursor-pointer hover:bg-[#026773] rounded-l-none bg-[#024959]
+     w-1/5 h-8 text-xl font-bold text-center text-white rounded"
+     >+</span>
+  </div>
+
+  
+<TaskComponent/>
+<TaskComponent/>
+
+
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ todo_tailwind/todo_tailwind/src/components/TaskComponent.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+    <div class="flex justify-between text-white bg-[#026773] px-4 py-1 mt-1">
+    Tarea 1
+    <span class="transition-colors duration-200 hover:cursor-pointer hover:bg-[#026773]  px-3 py-1 text-center text-white rounded">X</span>
+  </div>
+</template>
